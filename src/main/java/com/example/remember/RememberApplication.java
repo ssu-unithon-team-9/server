4db67fe9213397d7465609d466ab30c150c994b7
SSUN <rlatjsghks4647@naver.com>
2025-08-07 16:13:31 +0900

    init

---
+++ src/main/java/com/example/remember/RememberApplication.java
@@ -0,0 +1,13 @@
+package com.example.remember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberApplication.class, args);
+	}
+
+}
