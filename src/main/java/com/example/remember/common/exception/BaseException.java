430c27afd6d6e5030ae3e5e02b391793e26b8385
SSUN <rlatjsghks4647@naver.com>
2025-08-11 13:35:51 +0900

    [ADD] exception 추가

---
+++ src/main/java/com/example/remember/common/exception/BaseException.java
@@ -0,0 +1,28 @@
+package com.example.remember.common.exception;
+
+import com.example.remember.common.type.ErrorType;
+import org.springframework.http.HttpStatus;
+
+public class BaseException extends RuntimeException {
+    private final ErrorType errorType;
+    private final HttpStatus httpStatus;
+
+    public BaseException(ErrorType errorType, HttpStatus httpStatus) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
+
+    public int getHttpCode() {
+        return this.httpStatus.value();
+    }
+
+    public ErrorType getErrorType() {
+        return this.errorType;
+    }
+}
+
