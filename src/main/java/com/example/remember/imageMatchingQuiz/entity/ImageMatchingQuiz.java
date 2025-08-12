a9398c11d12bb844f8ff18f5dfa8abf64e5781ff
bricksky <skydh1214@naver.com>
2025-08-13 03:17:52 +0900

    feat: S3 이미지 3개 Presigned URL 조회 API 구현

---
+++ src/main/java/com/example/remember/imageMatchingQuiz/entity/ImageMatchingQuiz.java
@@ -0,0 +1,50 @@
+package com.example.remember.imageMatchingQuiz.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "image_matching_quiz")
+public class ImageMatchingQuiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, name = "key_0")
+    private String key0;
+
+    @NotBlank
+    @Column(nullable = false, name = "key_1")
+    private String key1;
+
+    @NotBlank
+    @Column(nullable = false, name = "key_2")
+    private String key2;
+
+    protected ImageMatchingQuiz() {
+    }
+
+    public ImageMatchingQuiz(String key0, String key1, String key2) {
+        this.key0 = key0;
+        this.key1 = key1;
+        this.key2 = key2;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getKey0() {
+        return key0;
+    }
+
+    public String getKey1() {
+        return key1;
+    }
+
+    public String getKey2() {
+        return key2;
+    }
+}
--- src/main/java/com/example/remember/imageMatchingQuiz/implement/PromptProvider.java
+++
@@ -1,4 +0,0 @@
-package com.example.remember.imageMatchingQuiz.implement;
-
-public class PromptProvider {
-}
