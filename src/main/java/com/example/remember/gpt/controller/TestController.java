1bf8ddfec56a0402ba287d6e3d445b032c9911ac
SSUN <rlatjsghks4647@naver.com>
2025-08-12 14:09:33 +0900

    fix : 코드 수정

--- src/main/java/com/example/remember/gpt/controller/TestController.java
+++ src/main/java/com/example/remember/gpt/controller/TestController.java
@@ -3,7 +3,7 @@ package com.example.remember.gpt.controller;
 import com.example.remember.common.dto.response.ApiResponse;
 import com.example.remember.common.type.gpt.GptSuccessType;
 import com.example.remember.gpt.dto.GPTResponseDto;
-import com.example.remember.gpt.service.ChatGPTServiceImpl;
+import com.example.remember.gpt.service.ChatGPTService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "GPT", description = "GPT test용입니다.")
 public class TestController {
 
-    private final ChatGPTServiceImpl chatGPTService;
+    private final ChatGPTService chatGPTService;
 
     @GetMapping("")
     @Operation(summary = "GPT 호출 API", description = "prompt 입력 시 gpt가 답변해줍니다.")
--- src/main/java/com/example/remember/gpt/service/ChatGPTService.java
+++ src/main/java/com/example/remember/gpt/service/ChatGPTService.java
@@ -1,9 +1,7 @@
 package com.example.remember.gpt.service;
 
 import com.example.remember.gpt.dto.GPTResponseDto;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface ChatGPTService {
     GPTResponseDto chat(String prompt);
 }
