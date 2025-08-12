867fef731c58e3ceff359feb45918119af956909
SSUN <rlatjsghks4647@naver.com>
2025-08-12 11:42:37 +0900

    feat : swagger 설명 추가

--- src/main/java/com/example/remember/profile/controller/ServerProfileController.java
+++ src/main/java/com/example/remember/profile/controller/ServerProfileController.java
@@ -1,5 +1,6 @@
 package com.example.remember.profile.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "서버 profile", description = "서버 profile 확인용입니다.")
 public class ServerProfileController {
     private final Environment env;
 
